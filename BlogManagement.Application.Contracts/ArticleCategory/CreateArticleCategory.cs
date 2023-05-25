cefeb1a33d36736642cdbeb4e164d13aaf7baf04
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2023-05-25 11:40:16 +0330

    implement ArticleCategory UI

--- BlogManagement.Application.Contracts/ArticleCategory/CreateArticleCategory.cs
+++ BlogManagement.Application.Contracts/ArticleCategory/CreateArticleCategory.cs
@@ -6,32 +6,32 @@ namespace BlogManagement.Application.Contracts.ArticleCategory
 {
     public class CreateArticleCategory
     {
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [Required(ErrorMessage = ValidationMessages.IsRequierd)]
         public string Name { get; set; }
         
         public IFormFile Picture { get; set; }
 
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [Required(ErrorMessage = ValidationMessages.IsRequierd)]
         public string PictureAlt { get; set; }
 
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [Required(ErrorMessage = ValidationMessages.IsRequierd)]
         public string PictureTitle { get; set; }
 
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [Required(ErrorMessage = ValidationMessages.IsRequierd)]
         public string Description { get; set; }
         
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [Required(ErrorMessage = ValidationMessages.IsRequierd)]
         public int ShowOrder { get; set; }
         
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [Required(ErrorMessage = ValidationMessages.IsRequierd)]
         public string Slug { get; set; }
 
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [Required(ErrorMessage = ValidationMessages.IsRequierd)]
         public string Keywords { get; set; }
 
-        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [Required(ErrorMessage = ValidationMessages.IsRequierd)]
         public string MetaDescription { get; set; }
 
-        public string CanonicalAddress { get; set; }
+        public string? CanonicalAddress { get; set; }
     }
 }
---
+++ ServiceHost/Areas/Administration/Controllers/Article/ArticleCategory/ArticleCategoryController.cs
@@ -0,0 +1,73 @@
+﻿using BlogManagement.Application.Contracts.ArticleCategory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic.CompilerServices;
+using ShopManagement.Application.Contracts.ProductCategory;
+
+namespace ServiceHost.Areas.Administration.Controllers.Article.ArticleCategory
+{
+    public class ArticleCategoryController : Controller
+    {
+        private readonly IArticleCategoryApplication _articleCategoryApplication;
+
+        private  List<ArticleCategoryViewModel> _articleCategoryViewModels = new List<ArticleCategoryViewModel> ();
+
+        public ArticleCategorySearchModel searchModel;
+
+        public ArticleCategoryController(IArticleCategoryApplication articleCategoryApplication)
+        {
+            _articleCategoryApplication = articleCategoryApplication;
+        }
+
+
+        [Area("Administration")]
+        [Route("admin/article/articlecategory/index")]
+        [HttpGet]
+        public IActionResult Index(string? searchModel)
+        {
+            var SearchModel = new ArticleCategorySearchModel()
+            {
+                Name = searchModel
+            };
+            _articleCategoryViewModels = _articleCategoryApplication.Search(SearchModel);
+            return View(_articleCategoryViewModels);
+        }
+
+
+        [Area("Administration")]
+        [Route("admin/article/articlecategory/Create")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return PartialView("_Create", new CreateArticleCategory());
+        }
+
+        [Area("Administration")]
+        [Route("admin/article/articlecategory/Create")]
+        [HttpPost]
+        public JsonResult Create(CreateArticleCategory commend)
+        {
+            var result = _articleCategoryApplication.Create(commend);
+
+            return new JsonResult(result);
+        }
+
+
+        [Area("Administration")]
+        [Route("admin/article/articlecategory/Edit")]
+        [HttpGet]
+        public IActionResult Edit(long Id)
+        {
+            var editArticleCategory = _articleCategoryApplication.GetDetails(Id);
+            return PartialView("_Edit", editArticleCategory);
+        }
+
+        [Area("Administration")]
+        [Route("admin/article/articlecategory/Edit")]
+        
+        public JsonResult Edit(EditArticleCategory commend)
+        {
+            var result = _articleCategoryApplication.Edit(commend);
+            return new JsonResult(result);
+        }
+    }
+}
