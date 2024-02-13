9295502724cff4d36917a1db2b7afbe94b4bd883
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-14 00:37:35 +0330

    refactor IProductCategoryApplication : change all method to async

--- ShopManagement.Applictation.Contracts/ProductCategory/IProductCategoryApplication.cs
+++ ShopManagement.Applictation.Contracts/ProductCategory/IProductCategoryApplication.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using _0_Framework.Application;
+﻿using _0_Framework.Application;
 
 
 namespace ShopManagement.Application.Contracts.ProductCategory
 {
 	public interface IProductCategoryApplication
 	{
-		OperationResult Create(CreateProductCategory command);
-		OperationResult Edit(EditProductCategory command);
-		ProductCategoryViewModel Get(long Id);
-        List<ProductCategoryViewModel> Get();
-		List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
-        EditProductCategory GetDetails(long Id);
-        List<ProductCategoryViewModel> GetProductCategories();
+		Task<OperationResult> Create(CreateProductCategory command);
+        Task<OperationResult> Edit(EditProductCategory command);
+		Task<ProductCategoryViewModel> Get(long id);
+        Task<List<ProductCategoryViewModel>> Get();
+		Task<List<ProductCategoryViewModel>> Search(ProductCategorySearchModel searchModel);
+        Task<EditProductCategory> GetDetails(long id);
+        Task<List<ProductCategoryViewModel>> GetProductCategories();
 
 
     }
