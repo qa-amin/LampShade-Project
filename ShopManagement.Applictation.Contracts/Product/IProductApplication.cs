414289a3cd64602eab86994f38c53b95c9833a62
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-14 10:36:14 +0330

    refactor IProductApplication : change all method to async

--- ShopManagement.Applictation.Contracts/Product/IProductApplication.cs
+++ ShopManagement.Applictation.Contracts/Product/IProductApplication.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using _0_Framework.Application;
+﻿using _0_Framework.Application;
 
 namespace ShopManagement.Application.Contracts.Product
 {
     public interface IProductApplication
     {
-        OperationResult Create(CreateProduct command);
-        OperationResult Edit(EditProduct command);
-        EditProduct GetDetails(long Id);
-        List<ProductViewModel> Search(ProductSearchModel searchModel);
-        //OperationResult IsStock(long Id);
-        //OperationResult NotInStock(long Id);
-        List<ProductViewModel> GetProducts();
+        Task<OperationResult> Create(CreateProduct command);
+        Task<OperationResult> Edit(EditProduct command);
+        Task<EditProduct> GetDetails(long id);
+        Task<List<ProductViewModel>> Search(ProductSearchModel searchModel);
+        Task<List<ProductViewModel>> GetProducts();
     }
 }
