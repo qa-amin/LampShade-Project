c094b711c33e3d84b590e6f329852951e3e2d421
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-14 17:17:44 +0330

    refactor DiscountRepository

--- CustomerDiscount.Infrastructure.EFCore/Repository/ColleagueDiscountRepository.cs
+++ CustomerDiscount.Infrastructure.EFCore/Repository/ColleagueDiscountRepository.cs
@@ -1,31 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using _0_Framework.Application;
 using _0_Framework.Infrastructure;
+using DiscountManagement.Application.Contracts.ColleagueDiscount;
 using DiscountManagement.Domain.ColleagueDiscountAgg;
 using Microsoft.EntityFrameworkCore;
+using ShopManagement.Infrastructure.EFCore;
 
 namespace DiscountManagement.Infrastructure.EFCore.Repository
 {
     public class ColleagueDiscountRepository : RepositoryBase<long, ColleagueDiscount>, IColleagueDiscountRepository
     {
         private readonly DiscountManagementDbContext _context;
+        private readonly ShopManagementDbContext _shopContext;
 
-        public ColleagueDiscountRepository(DiscountManagementDbContext context) : base(context)
+        public ColleagueDiscountRepository(DiscountManagementDbContext context, ShopManagementDbContext shopContext) : base(context)
         {
             _context = context;
+            _shopContext = shopContext;
         }
 
-        public List<ColleagueDiscount> Search(long id)
+        public async Task<List<ColleagueDiscountViewModel>> Search(long id)
         {
-            return _context.ColleagueDiscounts.ToList();
+            var products = await _shopContext.Products.Select(x => new { x.Id, x.Name }).ToListAsync();
+            var query = await _context.ColleagueDiscounts.Select(x => new ColleagueDiscountViewModel
+            {
+                Id = x.Id,
+                CreationDate = x.CreationDate.ToFarsi(),
+                DiscountRate = x.DiscountRate,
+                ProductId = x.ProductId,
+                IsRemoved = x.IsRemoved
+            }).ToListAsync();
+
+            if (id > 0)
+                query = query.Where(x => x.ProductId == id).ToList();
+
+            var discounts = query.OrderByDescending(x => x.Id).ToList();
+            discounts.ForEach(discount =>
+                discount.Product = products.FirstOrDefault(x => x.Id == discount.ProductId)?.Name);
+            return discounts;
         }
 
-        public ColleagueDiscount GetDetails(long id)
+        public async Task<EditColleagueDiscount> GetDetails(long id)
         {
-            return _context.ColleagueDiscounts.Find(id);
+            return await _context.ColleagueDiscounts.Select(x => new EditColleagueDiscount
+            {
+                Id = x.Id,
+                DiscountRate = x.DiscountRate,
+                ProductId = x.ProductId
+            }).FirstOrDefaultAsync(x => x.Id == id);
         }
     }
 }
--- CustomerDiscount.Infrastructure.EFCore/Repository/CustomerDiscountRepository.cs
+++ CustomerDiscount.Infrastructure.EFCore/Repository/CustomerDiscountRepository.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using _0_Framework.Application;
 using _0_Framework.Infrastructure;
+using DiscountManagement.Application.Contracts.CustomerDiscount;
 using DiscountManagement.Domain.CustomerDiscountAgg;
 using Microsoft.EntityFrameworkCore;
+using ShopManagement.Infrastructure.EFCore;
 
 
 namespace DiscountManagement.Infrastructure.EFCore.Repository
@@ -13,23 +13,64 @@ namespace DiscountManagement.Infrastructure.EFCore.Repository
     public class CustomerDiscountRepository : RepositoryBase<long , DiscountManagement.Domain.CustomerDiscountAgg.CustomerDiscount> , ICustomerDiscountRepository
     {
         private readonly DiscountManagementDbContext _context;
-        
-        public CustomerDiscountRepository(DiscountManagementDbContext context) : base(context)
+        private readonly ShopManagementDbContext _shopContext;
+
+        public CustomerDiscountRepository(DiscountManagementDbContext context, ShopManagementDbContext shopContext) : base(context)
         {
             _context = context;
-            
+            _shopContext = shopContext;
         }
 
 
-        public List<CustomerDiscount> search(long? productId, string? startDate, string? endDate)
+        public async Task<List<CustomerDiscountViewModel>> search(long? productId, string? startDate, string? endDate)
         {
-            
-            return _context.CustomerDiscounts.ToList();
+
+            var products = await _shopContext.Products.Select(x => new { x.Id, x.Name }).ToListAsync();
+            var query = _context.CustomerDiscounts.Select(x => new CustomerDiscountViewModel
+            {
+                Id = x.Id,
+                DiscountRate = x.DiscountRate,
+                EndDate = x.EndDate.ToFarsi(),
+                EndDateGr = x.EndDate,
+                StartDate = x.StartDate.ToFarsi(),
+                StartDateGr = x.StartDate,
+                ProductId = x.ProductId,
+                Reason = x.Reason,
+                CreationDate = x.CreationDate.ToFarsi()
+            });
+
+            if (productId > 0)
+                query = query.Where(x => x.ProductId == productId);
+
+            if (!string.IsNullOrWhiteSpace(startDate))
+            {
+                query = query.Where(x => x.StartDateGr > startDate.ToGeorgianDateTime());
+            }
+
+            if (!string.IsNullOrWhiteSpace(endDate))
+            {
+                query = query.Where(x => x.EndDateGr < endDate.ToGeorgianDateTime());
+            }
+
+            var discounts = query.OrderByDescending(x => x.Id).ToList();
+
+            discounts.ForEach(discount =>
+                discount.Product = products.FirstOrDefault(x => x.Id == discount.ProductId)?.Name);
+
+            return discounts;
         }
 
-        public CustomerDiscount GetDetails(long id)
+        public async Task<EditCustomerDiscount> GetDetails(long id)
         {
-            return _context.CustomerDiscounts.Find(id);
+            return await _context.CustomerDiscounts.Select(x => new EditCustomerDiscount()
+            {
+                Id = x.Id,
+                ProductId = x.ProductId,
+                DiscountRate = x.DiscountRate,
+                StartDate = x.StartDate.ToString(CultureInfo.InvariantCulture),
+                EndDate = x.EndDate.ToString(CultureInfo.InvariantCulture),
+                Reason = x.Reason
+            }).FirstOrDefaultAsync(x => x.Id == id);
         }
     }
 }
