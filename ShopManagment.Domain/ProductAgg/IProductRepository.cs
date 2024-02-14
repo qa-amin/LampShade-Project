b52ee16bbdda618c8829d86d37799365840562b6
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-14 10:16:10 +0330

    refactor IProductRepository : change methods to async

--- ShopManagment.Domain/ProductAgg/IProductRepository.cs
+++ ShopManagment.Domain/ProductAgg/IProductRepository.cs
@@ -4,17 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using _0_Framework.Domain;
+using ShopManagement.Application.Contracts.Product;
 
 namespace ShopManagement.Domain.ProductAgg
 {
     public interface IProductRepository : IRepository<long, Product>
     {
-        List<Product> Search(string? Name, string? Code, long? CategoryId);
-        
-
-        Product GetDetails(long Id);
-
-        List<Product> GetProducts();
-        public Product GetProductWithCategory(long id);
+        Task<List<ProductViewModel>> Search(string? name, string? code, long? categoryId);
+        Task<EditProduct> GetDetails(long id);
+        Task<List<ProductViewModel>> GetProducts();
+        Task<ProductViewModel> GetProductWithCategory(long id);
     }
 }
