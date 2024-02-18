a7ec9f5fd958a4ac29fb5f18e9986ec598c4c652
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-18 19:09:59 +0330

    refactor IOrderRepository and OrderRepository

--- ShopManagement.Infrastructure.EFCore/Repository/OrderRepository.cs
+++ ShopManagement.Infrastructure.EFCore/Repository/OrderRepository.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Data.Entity;
 using _0_Framework.Application;
 using _0_Framework.Infrastructure;
 using AccountManagement.Infrastructure.EFCore;
-using AccountMangement.Infrastructure.EFCore;
 using ShopManagement.Application.Contracts;
 using ShopManagement.Application.Contracts.Order;
 using ShopManagement.Domain.OrderAgg;
@@ -21,32 +19,74 @@ namespace ShopManagement.Infrastructure.EFCore.Repository
             _accountContext = accountContext;
         }
 
-        public double GetAmountBy(long id)
+        public async Task<double> GetAmountBy(long id)
         {
-            var order = _context.Orders
+            var order = await _context.Orders
                 .Select(x => new {x.PayAmount, x.Id})
-                .FirstOrDefault(x => x.Id == id);
+                .FirstOrDefaultAsync(x => x.Id == id);
             if (order != null)
                 return order.PayAmount;
             return 0;
         }
 
-        public List<OrderItem> GetItems(long orderId)
+        public async Task<List<OrderItemViewModel>> GetItems(long orderId)
         {
-            
-            var order = _context.Orders.FirstOrDefault(x => x.Id == orderId);
+
+            var products = _context.Products.Select(x => new { x.Id, x.Name });
+            var order = await _context.Orders.FirstOrDefaultAsync(x => x.Id == orderId);
             if (order == null)
-                return new List<OrderItem>();
+                return new List<OrderItemViewModel>();
 
-            var items = order.Items.ToList();
+            var items = order.Items.Select(x => new OrderItemViewModel
+            {
+                Id = x.Id,
+                Count = x.Count,
+                DiscountRate = x.DiscountRate,
+                OrderId = x.OrderId,
+                ProductId = x.ProductId,
+                UnitPrice = x.UnitPrice
+            });
 
-            return items;
+            foreach (var item in items)
+            {
+                var product = await products.FirstOrDefaultAsync(x => x.Id == item.ProductId);
+                item.Product = product.Name;
+            }
+
+            return  items.ToList();
         }
 
 
-        public List<Order> Search(long ?AccountId, bool? isCanceled)
+        public async Task<List<OrderViewModel>> Search(long ?accountId, bool? isCanceled)
         {
-            return _context.Orders.ToList();
+            var accounts =  _accountContext.Accounts.Select(x => new { x.Id, x.Fullname });
+            var query = _context.Orders.Select(x => new OrderViewModel
+            {
+                Id = x.Id,
+                AccountId = x.AccountId,
+                DiscountAmount = x.DiscountAmount,
+                IsCanceled = x.IsCanceled,
+                IsPaid = x.IsPaid,
+                IssueTrackingNo = x.IssueTrackingNo,
+                PayAmount = x.PayAmount,
+                PaymentMethodId = x.PaymentMethod,
+                RefId = x.RefId,
+                TotalAmount = x.TotalAmount,
+                CreationDate = x.CreationDate.ToFarsi()
+            });
+
+            if (isCanceled != null) query = query.Where(x => x.IsCanceled == isCanceled);
+
+            if (accountId > 0) query = query.Where(x => x.AccountId == accountId);
+
+            var orders = query.OrderByDescending(x => x.Id);
+            foreach (var order in orders)
+            {
+                order.AccountFullName = accounts.FirstOrDefault(x => x.Id == order.AccountId)?.Fullname;
+                order.PaymentMethod = PaymentMethod.GetBy(order.PaymentMethodId).Name;
+            }
+
+            return await orders.ToListAsync();
         }
     }
 }
--- ShopManagment.Domain/OrderAgg/IOrderRepository.cs
+++ ShopManagment.Domain/OrderAgg/IOrderRepository.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using _0_Framework.Domain;
+using ShopManagement.Application.Contracts.Order;
 
 
 namespace ShopManagement.Domain.OrderAgg
 {
     public interface IOrderRepository : IRepository<long, Order>
     {
-        double GetAmountBy(long id);
-        List<OrderItem> GetItems(long orderId);
-        List<Order> Search(long? AccountId,bool? isCanceled);
+        Task<double> GetAmountBy(long id);
+        Task<List<OrderItemViewModel>> GetItems(long orderId);
+        Task<List<OrderViewModel>> Search(long? accountId,bool? isCanceled);
     }
 }
