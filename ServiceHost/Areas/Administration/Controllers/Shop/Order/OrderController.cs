dcea6b77107666406debb0f3993fb4e45a7b2b7e
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-18 19:50:14 +0330

    refactor Order End Point

--- ServiceHost/Areas/Administration/Controllers/Shop/Order/OrderController.cs
+++ ServiceHost/Areas/Administration/Controllers/Shop/Order/OrderController.cs
@@ -1,6 +1,4 @@
-﻿using System.Web.Mvc;
-using AccountManagement.Application.Contracts.Account;
-using DocumentFormat.OpenXml.Office2010.Excel;
+﻿using AccountManagement.Application.Contracts.Account;
 using Microsoft.AspNetCore.Mvc;
 using ShopManagement.Application.Contracts.Order;
 using Controller = Microsoft.AspNetCore.Mvc.Controller;
@@ -20,45 +18,45 @@ namespace ServiceHost.Areas.Administration.Controllers.Shop.Order
             _orderApplication = orderApplication;
         }
         [Area("Administration")]
-        [Microsoft.AspNetCore.Mvc.Route("admin/shop/order/index")]
-        [System.Web.Mvc.HttpGet]
-        public IActionResult Index(long? accountId, bool? isCanceled)
+        [Route("admin/orders")]
+        [HttpGet]
+        public async Task<IActionResult> Index(long? accountId, bool? isCanceled)
         {
             var searchModel = new OrderSearchModel
             {
                 AccountId = accountId,
                 IsCanceled = isCanceled
             };
-            var accounts = _accountApplication.GetAccounts();
+            var accounts = await _accountApplication.GetAccounts();
             ViewBag.Accounts = accounts;
 
-            var order = _orderApplication.Search(searchModel);
+            var order = await _orderApplication.Search(searchModel);
             return View(order);
         }
         [Area("Administration")]
-        [Microsoft.AspNetCore.Mvc.Route("admin/shop/order/Cancel")]
-        [System.Web.Mvc.HttpGet]
-        public IActionResult Cancel(long id)
+        [Route("admin/order/Cancel")]
+        [HttpGet]
+        public async Task<IActionResult> Cancel(long id)
         {
-            _orderApplication.Cancel(id);
+            await _orderApplication.Cancel(id);
 
             return RedirectToAction("Index","Order",new{AdminArea = "Administration" });
         }
         [Area("Administration")]
-        [Microsoft.AspNetCore.Mvc.Route("admin/shop/order/Confirm")]
-        [System.Web.Mvc.HttpGet]
-        public IActionResult Confirm(long id)
+        [Route("admin/order/Confirm")]
+        [HttpGet]
+        public async Task<IActionResult> Confirm(long id)
         {
-            _orderApplication.PaymentSucceeded(id, 0);
+            await _orderApplication.PaymentSucceeded(id, 0);
 
             return RedirectToAction("Index");
         }
         [Area("Administration")]
-        [Microsoft.AspNetCore.Mvc.Route("admin/shop/order/Items")]
-        [System.Web.Mvc.HttpGet]
-        public IActionResult Items(long id)
+        [Route("admin/order/Items")]
+        [HttpGet]
+        public async Task<IActionResult> Items(long id)
         {
-            var items = _orderApplication.GetItems(id);
+            var items = await _orderApplication.GetItems(id);
             return PartialView("Items", items);
         }
     }
