1e0da84cf9024220326471d2a4835f3086c9d60e
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-18 20:12:23 +0330

    refactor SearchController

--- ServiceHost/Controllers/SearchController.cs
+++ ServiceHost/Controllers/SearchController.cs
@@ -1,25 +1,21 @@
 ﻿using _1_LampShadeQuery.Contracts.Product;
 using Microsoft.AspNetCore.Mvc;
-using ShopManagement.Domain.ProductAgg;
-
 namespace ServiceHost.Controllers
 {
     public class SearchController : Controller
     {
         private readonly IProductQuery _productQuery;
-        public List<ProductQueryModel>? Products { get; set; }
-
         public SearchController(IProductQuery productQuery)
         {
             _productQuery = productQuery;
         }
 
         [HttpPost]
-        [Route("search/index")]
-        public IActionResult Index(string id)
+        [Route("search")]
+        public async Task<IActionResult> Index(string id)
         {
-            Products = _productQuery.Search(id);
-            ViewBag.Products = Products;
+            var products = await _productQuery.Search(id);
+            ViewBag.Products = products;
             ViewBag.Search = id;
             return View();
         }
