fdaaab0877d9c3f5b6da9754402808826f9088c7
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-18 17:11:04 +0330

    refactor IArticleRepository

--- BlogManagement.Domain/ArticleAgg/IArticleRepository.cs
+++ BlogManagement.Domain/ArticleAgg/IArticleRepository.cs
@@ -1,13 +1,12 @@
 ﻿using _0_Framework.Domain;
 using BlogManagement.Application.Contracts.Article;
-using System.Collections.Generic;
 
 namespace BlogManagement.Domain.ArticleAgg
 {
     public interface IArticleRepository : IRepository<long, Article>
     {
-        EditArticle GetDetails(long id);
-        Article GetWithCategory(long id);
-        List<ArticleViewModel> Search(ArticleSearchModel searchModel);
+        Task<EditArticle> GetDetails(long id);
+        Task<Article> GetWithCategory(long id);
+        Task<List<ArticleViewModel>> Search(ArticleSearchModel searchModel);
     }
 }
