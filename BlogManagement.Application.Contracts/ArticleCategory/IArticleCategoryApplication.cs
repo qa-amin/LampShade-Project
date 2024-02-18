f90516c7107af01fccb740f8ee2fb3b0054553b1
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-18 17:15:50 +0330

    refactor IArticleCategoryApplication

--- BlogManagement.Application.Contracts/ArticleCategory/IArticleCategoryApplication.cs
+++ BlogManagement.Application.Contracts/ArticleCategory/IArticleCategoryApplication.cs
@@ -1,14 +1,13 @@
 ﻿using _0_Framework.Application;
-using System.Collections.Generic;
 
 namespace BlogManagement.Application.Contracts.ArticleCategory
 {
     public interface IArticleCategoryApplication
     {
-        OperationResult Create(CreateArticleCategory command);
-        OperationResult Edit(EditArticleCategory command);
-        EditArticleCategory GetDetails(long id);
-        List<ArticleCategoryViewModel> GetArticleCategories();
-        List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel);
+        Task<OperationResult> Create(CreateArticleCategory command);
+        Task<OperationResult> Edit(EditArticleCategory command);
+        Task<EditArticleCategory> GetDetails(long id);
+        Task<List<ArticleCategoryViewModel>> GetArticleCategories();
+        Task<List<ArticleCategoryViewModel>> Search(ArticleCategorySearchModel searchModel);
     }
 }
