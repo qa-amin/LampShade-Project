df5bd18ecda2c6c8e29246a0b653ff26c7e330ff
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-04-07 18:47:28 +0330

    add unit test for ProductCategory Constructor

---
+++ test/ShopManagement/SM.Domain/ShopManagment.Domain.Tests.Unit/ProductCategoryAgg/ProductCategoryBuilder.cs
@@ -0,0 +1,26 @@
+﻿using ShopManagement.Domain.ProductAgg;
+using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace ShopManagment.Domain.Tests.Unit.ProductCategoryAgg
+{
+    internal class ProductCategoryBuilder
+    {
+        public string Name { get; private set; } = "Book";
+        public string Description { get; private set; } = "This is a good picture";
+        public string Picture { get; private set; } = "نگهدارنده/2023-07-21-14-03-06-Product05.jpg";
+        public string PictureAlt { get; private set; } = "Book Alt";
+        public string PictureTitle { get; private set; } = "Book Title";
+        public string KeyWords { get; private set; } = "Book KeyWords";
+        public string MetaDescription { get; private set; } = "Book MetaDescription";
+        public string Slug { get; private set; } = "Book Slug";
+        public ProductCategory Build()
+        {
+            return new ProductCategory(Name, Description, 
+                Picture, PictureAlt, PictureTitle, KeyWords, MetaDescription,
+                Slug);
+        }
+
+        
+
+    }
+}
---
+++ test/ShopManagement/SM.Domain/ShopManagment.Domain.Tests.Unit/ProductCategoryAgg/ProductCategoryTest.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace ShopManagment.Domain.Tests.Unit.ProductCategoryAgg
+{
+    public class ProductCategoryTest
+    {
+        [Fact]
+        public void Constructor_ShouldConstructProductCategoryProperty()
+        {
+            var productCategoryBuilder = new ProductCategoryBuilder();
+            var productCategory = productCategoryBuilder.Build();
+
+
+            productCategory.Name.Should().Be(productCategoryBuilder.Name);
+            productCategory.Description.Should().Be(productCategoryBuilder.Description);
+            productCategory.Picture.Should().Be(productCategoryBuilder.Picture);
+            productCategory.PictureAlt.Should().Be(productCategoryBuilder.PictureAlt);
+            productCategory.PictureTitle.Should().Be(productCategoryBuilder.PictureTitle);
+            productCategory.KeyWords.Should().Be(productCategoryBuilder.KeyWords);
+            productCategory.MetaDescription.Should().Be(productCategory.MetaDescription);
+            productCategory.Slug.Should().Be(productCategoryBuilder.Slug);
+
+        }
+
+        [Fact]
+        public void Edit_ShouldChangeNameProperty()
+        {
+            var productCategoryBuilder = new ProductCategoryBuilder();
+            var productCategory = productCategoryBuilder.Build();
+
+            var newName = "Book1";
+
+            productCategory.Edit(newName, productCategoryBuilder.Description,
+                productCategoryBuilder.Picture, productCategoryBuilder.PictureAlt,
+                productCategoryBuilder.PictureTitle, productCategoryBuilder.KeyWords,
+                productCategoryBuilder.MetaDescription,
+                productCategoryBuilder.Slug);
+
+            productCategory.Name.Should().NotBe(productCategoryBuilder.Name);
+            productCategory.Name.Should().Be(newName);
+        }
+    }
+}
