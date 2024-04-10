36a186b1f8a48efaae2fd8ba057579fa6856f42e
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-04-10 13:36:07 +0330

    add unit test for all ProductCategoryRepository Methodes

--- test/ShopManagement/SM.Infrasturcture/ShopManagment.Infrastructure.Tests.Unit/Repositories/ProductCategoryRepositoryTest.cs
+++ test/ShopManagement/SM.Infrasturcture/ShopManagment.Infrastructure.Tests.Unit/Repositories/ProductCategoryRepositoryTest.cs
@@ -1,6 +1,6 @@
 ﻿using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Identity.Client;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
 using ShopManagement.Infrastructure.EFCore;
 using ShopManagement.Infrastructure.EFCore.Repository;
 using ShopManagment.Domain.Tests.Unit.ProductCategoryAgg;
@@ -45,5 +45,231 @@ namespace ShopManagment.Infrastructure.Tests.Unit.Repositories
 
         }
 
+
+        [Fact]
+        public async Task Create_ShouldNotBeEqual_WhenIdIsDifferent()
+        {
+            //Arrange
+            var productCategory1 = _productCategoryBuilder.Build();
+            var productCategory2 = _productCategoryBuilder.Build();
+
+
+            //Act
+            await _productCategoryRepository.Create(productCategory1);
+            await _productCategoryRepository.Create(productCategory2);
+            await _productCategoryRepository.SaveChanges();
+            var productCategoryDb1 = await _context.ProductCategories.FirstOrDefaultAsync();
+            var productCategoryDb2 = await _context.ProductCategories.LastOrDefaultAsync();
+
+            //Assert
+            productCategory1.Id.Should().NotBe(productCategoryDb2.Id);
+
+
+        }
+
+        [Fact]
+        public async Task Get_ShouldReturnListOfAllProductCategoryFromDb()
+        {
+            //Arrange
+            var productCategory1 = _productCategoryBuilder.Build();
+            var productCategory2 = _productCategoryBuilder.Build();
+            var productCategory3 = _productCategoryBuilder.Build();
+
+            //Act
+            await _productCategoryRepository.Create(productCategory1);
+            await _productCategoryRepository.Create(productCategory2);
+            await _productCategoryRepository.Create(productCategory3);
+            await _productCategoryRepository.SaveChanges();
+            var productCategories = await _productCategoryRepository.Get();
+
+
+            //Assert
+            productCategories.Count.Should().BeGreaterThanOrEqualTo(3);
+
+
+        }
+
+        [Fact]
+        public async Task Get_ShouldReturnProductCategory_WhenGivesId()
+        {
+            //Arrange
+            var productCategory = _productCategoryBuilder.Build();
+
+            //Act
+            await _productCategoryRepository.Create(productCategory);
+            await _productCategoryRepository.SaveChanges();
+            var productCategoryWithIdOne = await _productCategoryRepository.Get(1);
+
+            //Assert
+            productCategoryWithIdOne.Id.Should().Be(productCategory.Id);
+            
+            
+        }
+
+        [Fact]
+        public async Task GetSlugById_ShouldReturnSlugOfProductCategory_WhenGivesId()
+        {
+            //Arrange
+            var productCategory =  _productCategoryBuilder.Build();
+
+            
+            //Act
+            await _productCategoryRepository.Create(productCategory);
+            await _productCategoryRepository.SaveChanges();
+
+            var slug = await _productCategoryRepository.GetSlugById(1);
+
+            //Assert
+            slug.Should().Be(productCategory.Slug);
+        }
+
+        [Fact]
+        public async Task Search_ShouldReturnProductCategory_WhenGivesSearchModelLikeBook()
+        {
+            //Arrange
+            var productCategory = _productCategoryBuilder.Build();
+
+
+            //Act
+            await _productCategoryRepository.Create(productCategory);
+            await _productCategoryRepository.SaveChanges();
+
+            var productCategoryWithSearch = await _productCategoryRepository.Search("Book");
+
+            //Assert
+            productCategoryWithSearch.Count.Should().Be(1);
+
+        }
+
+        [Fact]
+        public async Task Search_ShouldReturnListOfProductCategory_WhenGivesSearchModelLikeBook()
+        {
+            //Arrange
+            var productCategory1 = _productCategoryBuilder.Build();
+            var productCategory2 = _productCategoryBuilder.Build();
+
+
+            //Act
+            await _productCategoryRepository.Create(productCategory1);
+            await _productCategoryRepository.Create(productCategory2);
+            await _productCategoryRepository.SaveChanges();
+
+            var productCategoryWithSearch = await _productCategoryRepository.Search("Book");
+
+            //Assert
+            productCategoryWithSearch.Count.Should().Be(2);
+
+        }
+
+        [Fact]
+        public async Task Search_ShouldNotReturnProductCategoryOrListOfProductCategory_WhenGivesSearchModelLikePen()
+        {
+            //Arrange
+            var productCategory1 = _productCategoryBuilder.Build();
+            var productCategory2 = _productCategoryBuilder.Build();
+
+
+            //Act
+            await _productCategoryRepository.Create(productCategory1);
+            await _productCategoryRepository.Create(productCategory2);
+            await _productCategoryRepository.SaveChanges();
+
+            var productCategoryWithSearch = await _productCategoryRepository.Search("Pen");
+
+            //Assert
+            productCategoryWithSearch.Count.Should().Be(0);
+
+        }
+
+        [Fact]
+        public async Task Exists_ShouldReturnTrue_WhenGivesNameThatExistsInDb()
+        {
+            //Arrange
+            var productCategory = _productCategoryBuilder.Build();
+            const string name = "Book";
+
+            //Act
+            await _productCategoryRepository.Create(productCategory);
+            await _productCategoryRepository.SaveChanges();
+
+            var exists = await _productCategoryRepository.Exists(x => x.Name == name);
+
+            //Assert
+            exists.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task Exists_ShouldReturnFalse_WhenGivesNameThatNotExistsInDb()
+        {
+            //Arrange
+            var productCategory = _productCategoryBuilder.Build();
+            const string name = "Book1";
+
+            //Act
+            await _productCategoryRepository.Create(productCategory);
+            await _productCategoryRepository.SaveChanges();
+
+            var exists = await _productCategoryRepository.Exists(x => x.Name == name);
+
+            //Assert
+            exists.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task Exists_ShouldReturnFalse_WhenGivesNameThatExistsWithDifferentIdInDb()
+        {
+            //Arrange
+            var productCategory = _productCategoryBuilder.Build();
+            const int id = 1;
+            const string name = "Book";
+
+
+            //Act
+            await _productCategoryRepository.Create(productCategory);
+            await _productCategoryRepository.SaveChanges();
+
+            var exists = await _productCategoryRepository.Exists(x => x.Name == name && x.Id != id);
+
+            //Assert
+            exists.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task GetDetails_ShouldReturnEditProductCategory_WhenGivesId()
+        {
+            //Arrange
+            var productCategory = _productCategoryBuilder.Build();
+
+
+            //Act
+            await _productCategoryRepository.Create(productCategory);
+            await _productCategoryRepository.SaveChanges();
+
+            var editProductCategory = await _productCategoryRepository.GetDetails(productCategory.Id);
+
+
+            //Assert
+            editProductCategory.Id.Should().Be(productCategory.Id);
+            editProductCategory.Slug.Should().Be(productCategory.Slug);
+        }
+
+        [Fact]
+        public async Task GetProductCategories_ShouldReturnListOfProductCategoryViewModel()
+        {
+            //Arrange
+            var productCategory1 = _productCategoryBuilder.Build();
+            var productCategory2 = _productCategoryBuilder.Build();
+
+
+            //Act
+            await _productCategoryRepository.Create(productCategory1);
+            await _productCategoryRepository.Create(productCategory2);
+            await _productCategoryRepository.SaveChanges();
+
+            var listProductCategoryViewModel = await _productCategoryRepository.GetProductCategories();
+
+            //Assert
+            listProductCategoryViewModel.Should().HaveCountGreaterThanOrEqualTo(2);
+        }
     }
 }
